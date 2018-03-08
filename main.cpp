d3425330381fd2454ee0c2f0a9bbe5572173e324
Du Park <dupark3@gmail.com>
2018-03-08 06:07:28 -0500

    Added bidirectional iterator test in main function

--- main.cpp
+++ main.cpp
@@ -14,19 +14,27 @@ Test the class by rewriting the split function to put its output into a String_l
 #include <iostream>
 
 #include "String_list.h"
+#include "split.h"
 
 int main(int argc, char **argv)
 {
     // initialize an object of my class String_list
-    String_list myObject;
+    String_list myStringList1;
 
     // store arguments into my object
-    for (int i = 1; i < argc; ++i)
-        myObject.push_back(argv[i]);
+    for (size_t i = 1; i < argc; ++i)
+        myStringList1.push_back(argv[i]);
 
     // print the results
-    for (int i = 0; i < myObject.size(); ++i)
-        std::cout << myObject.access(i) << std::endl;
+    for (size_t i = 0; i < myStringList1.size(); ++i)
+        std::cout << myStringList1.access(i) << std::endl;
+
+    // accessing bidirectional iterator
+    std::cout << std::endl << std::endl;
+    std::cout << *myStringList1.begin() << std::endl;
+    std::cout << *myStringList1.begin()++ << std::endl;
+    std::cout << *(myStringList1.end() - 1) << std::endl;
+
 
     return 0;
 }
